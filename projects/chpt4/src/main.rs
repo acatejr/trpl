aaf079d4deb861a0012f1cbd8b8befcac811f057
acatejr <acatejr@gmail.com>
2024-09-17 09:46:39 -0700

    More work on chapter 4

--- projects/chpt4/src/main.rs
+++ projects/chpt4/src/main.rs
@@ -2,4 +2,19 @@ fn main() {
     let mut s = String::from("Hello, world!");
     s.push_str(", word");
     println!("{s}");
+
+    let s_len = calc_len(&s);
+    println!("The string length is: {s_len}");
+    println!("{s}");
+
+    change(&mut s);
+    println!("{s}");
+}
+
+fn calc_len(s: &String) -> usize {
+    s.len()
+}
+
+fn change(some_str: &mut String) {
+    some_str.push_str(", world world");
 }
